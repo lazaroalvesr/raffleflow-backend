ff0f974786c945f7b2d9040d0944f5873e113112
lazaro <lazaroalves12355@gmail.com>
2024-11-23 15:24:46 -0300

    032

--- src/main.ts
+++ src/main.ts
@@ -15,15 +15,13 @@ async function bootstrap() {
   app.useGlobalGuards(new JwtAuthGuard(reflector));
 
   app.enableCors({
-    origin: [
-      'https://raffle-master-front.vercel.app'
-    ],
-    credentials: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: ['Content-Type', 'Accept', 'Authorization', 'X-CSRF-Token'],
-    exposedHeaders: ['X-CSRF-Token'],
+    origin: ['https://raffle-master-front.vercel.app'], // Certifique-se de que a origem está correta
+    credentials: true, // Permite envio de cookies e autenticação com credenciais
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // Métodos HTTP permitidos
+    allowedHeaders: ['Content-Type', 'Accept', 'Authorization', 'X-CSRF-Token'], // Cabeçalhos permitidos
+    exposedHeaders: ['X-CSRF-Token'], // Cabeçalhos que o navegador pode acessar
   });
-
+  
 
   app.use(helmet());
 
