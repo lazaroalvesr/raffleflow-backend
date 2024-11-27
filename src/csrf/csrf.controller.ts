9a8b0cfccda561c14ae43b6203ce56c70cb026aa
lazaro <lazaroalves12355@gmail.com>
2024-11-27 13:10:29 -0300

    KKKK

--- src/csrf/csrf.controller.ts
+++ src/csrf/csrf.controller.ts
@@ -4,17 +4,7 @@ import { Controller, Get, Req, Res } from '@nestjs/common';
 export class CsrfController {
   @Get('token')
   getCsrfToken(@Req() req, @Res() res) {
-    // Gera e retorna o token CSRF
-    const csrfToken = req.csrfToken();
-    
-    // Define o cookie com o token CSRF
-    res.cookie('XSRF-TOKEN', csrfToken, {
-      httpOnly: false, // Acessível por JavaScript no frontend
-      sameSite: 'strict', 
-      secure: process.env.NODE_ENV === 'production', // Só ativa o `secure` em produção
-    });
-
-    // Retorna o token CSRF no corpo da resposta
-    res.json({ csrfToken });
+    // O middleware já cuida da geração do token
+    res.json({ message: 'Token disponível' });
   }
 }
