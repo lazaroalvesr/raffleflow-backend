506fc8a9e144ba390367400dbaba3af00b11acd4
lazaro <lazaroalves12355@gmail.com>
2024-12-07 09:39:23 -0300

    modified fews lines in file DockerFile

--- wait-for-db.sh
+++ wait-for-db.sh
@@ -1,4 +1,3 @@
-# Use full path to pg_isready if needed
 until /usr/bin/pg_isready -h db -p 5432 -U "$DB_USER"; do
   echo "Waiting for database to be ready..."
   sleep 2
@@ -7,5 +6,4 @@ done
 echo "Database is ready. Running Prisma migrations..."
 npx prisma migrate deploy
 
-# Start the application
 node dist/main
