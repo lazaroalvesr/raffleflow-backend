652bdb1b0c968d6c043360b3188842dc85b4da40
lazaro <lazaroalves12355@gmail.com>
2025-01-07 13:54:52 -0300

    editing route paymentAll

--- src/payment/payment.controller.ts
+++ src/payment/payment.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Query, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
 import { PaymentService } from './payment.service';
+import { AdminGuard } from 'src/lib/AdmGuard';
+import { SearchUserPaymentDTO } from 'src/dto/payment/searchUserDTO';
 
 @Controller('payment')
 export class PaymentController {
@@ -11,7 +13,9 @@ export class PaymentController {
     }
 
     @Get("getPaymentInfoAll")
-    async getPaymentInfo() {
-        return await this.paymentService.getAllPaymentInfoAll()
+    @UseGuards(AdminGuard)
+    @UsePipes(new ValidationPipe({ transform: true }))
+    async getPaymentInfo(@Query() filters: SearchUserPaymentDTO) {
+        return await this.paymentService.getAllPaymentInfoAll(filters)
     }
 }
